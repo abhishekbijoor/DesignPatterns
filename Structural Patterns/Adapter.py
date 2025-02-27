e86e8e2c208d633ab5a66dc0c3bb1dce3edaa5f9
Abhishek N Bijoor <42459892+abhishekbijoor@users.noreply.github.com>
2025-02-26 19:25:42 -0600

    Added Adapter design pattern

---
+++ Structural Patterns/Adapter.py
@@ -0,0 +1,34 @@
+# 🔹 Imagine you have a USB keyboard (send_keystrokes()) but need to make it work with a Bluetooth device (transmit_data()).
+# 🔹 Implement an Adapter to make them compatible.
+
+class USBKeyBoard:
+    alphabets = []
+    def __init__(self,type,language):
+        print(f"class name : {__name__}")
+        self.type = type
+        self.language = language
+    
+    def send_keystrokes(self, alphabet):
+        self.alphabets.append(alphabet)
+
+
+class BluetoothDevice:
+    def transmit_data(self,data):
+        pass
+
+class BluetoothAdapter(BluetoothDevice):
+    def __init__(self,usbdevice):
+        self.usbdevice = usbdevice
+
+    def transmit_data(self, data):
+        print(f"class name : {__name__}")
+
+        self.usbdevice.send_keystrokes(data)
+        print(self.usbdevice.alphabets)
+
+if __name__== "__main__":
+    usbdevice = USBKeyBoard("USB","English")
+    bluetoothAdapter = BluetoothAdapter(usbdevice)
+
+    bluetoothAdapter.transmit_data("abc")
+    bluetoothAdapter.transmit_data("cde")
